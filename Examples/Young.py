2b9de12ab2141f91cbf500b1594a7d1b0df1769e
FredvanGoor <fred511949@gmail.com>
2015-11-22 12:13:56 +0100

    Update Young.py

--- Examples/Young.py
+++ Examples/Young.py
@@ -22,7 +22,7 @@ try:
 	F1=LP.CircAperture(0.15*mm, 0, -0.6*mm, F)
 	F2=LP.CircAperture(0.15*mm, 0, 0.6*mm, F)    
 	F=LP.BeamMix(F1,F2)
-	F=LP.Fresnel(0.0,F)
+	F=LP.Fresnel(50*cm,F)
 	I=LP.Intensity(2,F)
 	plt.imshow(I); plt.axis('off');plt.title('intensity pattern')
 	plt.show()
