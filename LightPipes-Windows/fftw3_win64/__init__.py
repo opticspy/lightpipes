ec0108f32090a25c237435e16ff18cc9e596691b
FredvanGoor <fred511949@gmail.com>
2017-02-16 23:27:03 +0100

    deleted __init__.py

--- LightPipes-Windows/fftw3_win64/__init__.py
+++
@@ -1,6 +0,0 @@
-import site; site.getsitepackages()
-import ctypes
-#print("loading libfftw3-3.dll")
-ctypes.WinDLL(site.getsitepackages()[1] + "\\fftw3_win64\\libfftw3-3.dll")
-#print('libfftw3-3.dll loaded')
-
