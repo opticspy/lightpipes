70f09b029d177f439d0287e7ba1be7f005620517
FredvanGoor <fred511949@gmail.com>
2017-02-23 12:13:07 +0100

    edit Zernike example

--- Examples/Commands/Zernike.py
+++ Examples/Commands/Zernike.py
@@ -1,9 +1,13 @@
+#!/usr/bin/python
+"""
+This example demonstrates the Zernike command.
+..  :copyright: (c) 2017 by Fred van Goor.
+    :license: MIT, see License for more details.
+"""
+
 import LightPipes
 import matplotlib.pyplot as plt
-from scipy import misc
-#import matplotlib.image as mpimg
-#import numpy as np
-#from mpl_toolkits.mplot3d import Axes3D
+
 m=1
 nm=1e-9*m
 um=1e-6*m
@@ -15,14 +19,17 @@ LP=LightPipes.Init()
 wavelength=500*nm
 size=2.0*mm
 N=200
-nz=8
-mz=-2
+
+#Zernike 
+mz=-3 #azimuthal order
+nz=5 #radial order
 
 F=LP.Begin(size,wavelength,N)
 F=LP.Zernike(nz,mz,size/2,wavelength/(2*pi),F)
 F=LP.CircAperture(size/2,0,0,F)
 Phi=LP.Phase(F)
 plt.imshow(Phi)
-plt.title(print( "{\\fontsize{10}Z^{%d}_{%d}}  \n",mz,nz))
+s='Zernike Polynomial: $Z^{'+repr(mz)+'}_{'+repr(nz)+'}$'
+plt.title(s);plt.axis('off')
 plt.show()
 
