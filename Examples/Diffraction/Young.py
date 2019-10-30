5d185b3a071c752ae731f9938241e43903f9ece1
FredvanGoor <fred511949@gmail.com>
2019-10-30 15:07:40 +0100

    Update Young.py

--- Examples/Diffraction/Young.py
+++ Examples/Diffraction/Young.py
@@ -1,20 +1,20 @@
 #Interference from two holes.
 import matplotlib.pyplot as plt
-import LightPipes
-from LightPipes import cm, m, mm, nm, um
 
-
-LP=LightPipes.Init()
+from LightPipes import *
 
 wavelength=20*um
 size=30.0*mm
-N=1000
-LP.version()
-F=LP.Begin(size,wavelength,N)
-F1=LP.CircAperture(0.15*mm,  -0.6*mm,0, F)
-F2=LP.CircAperture(0.15*mm,  0.6*mm,0, F)
-F=LP.BeamMix(F1,F2)
-F=LP.Fresnel(30.0*cm,F)
-I=LP.Intensity(2,F)
+N=200
+print(LPversion)
+#F=Begin(size,wavelength,N)
+#F1=CircAperture(0.15*mm,  -0.6*mm,0, F)
+#F2=CircAperture(0.15*mm,  0.6*mm,0, F)
+#Use the PointSource command for versions >=1.2.0
+F1=PointSource(size,wavelength,N,-0.6*mm,0)
+F2=PointSource(size,wavelength,N,0.6*mm,0)
+F=BeamMix(F1,F2)
+F=Fresnel(30.0*cm,F)
+I=Intensity(2,F)
 plt.imshow(I); plt.axis('off');plt.title('intensity pattern')
 plt.show()
