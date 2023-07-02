1ba11b8742bcddf71fce25c455de9634bcd68eed
Leonard.Doyle <leonard.doyle@physik.uni-muenchen.de>
2023-07-02 17:52:46 +0200

    Improved plotutils and added PlotIntensity PlotPhase commands

--- LightPipes/plotutils.py
+++ LightPipes/plotutils.py
@@ -6,7 +6,8 @@ import numpy as _np
 from .units import *
 from .core import Phase, PhaseUnwrap, Intensity
 
-def Plot(F, unwrap = False, phaseblank =True, title='', circ_r=None, zoom=1):
+def Plot(F, unwrap = False, phaseblank =True, title='', circ_r=None, zoom=1,
+         ph_units='rad', figsize = None):
     """
     Plot the field intensity and phase with matplotlib.
 
@@ -22,10 +23,30 @@ def Plot(F, unwrap = False, phaseblank =True, title='', circ_r=None, zoom=1):
         DESCRIPTION. The default is ''.
     circ_c : float, optional
         [m] radius of a circle to draw. The default is None, i.e. circle off.
-
+    :param ph_units: Units to plot phase in (rad, opd, lam, default=rad)
     Returns
     -------
-    None.
+    (fig, (ax_left, ax_right)) handles in a tuple.
+
+    """
+    
+    import matplotlib.pyplot as plt
+    
+    fig, axs = plt.subplots(1, 2, sharex=True, sharey=True, figsize=figsize)
+    ax1, ax2 = axs
+    
+    if title:
+        fig.suptitle(title)
+    PlotIntensity(F, circ_r, zoom, ax=ax1)
+    PlotPhase(F, unwrap, phaseblank, circ_r, zoom, ph_units= ph_units, ax=ax2)
+    return fig, axs
+
+
+def PlotIntensity(F, circ_r=None, zoom=1, title='',
+         ax=None, **kwargs):
+    """
+    Plot the field intensity with matplotlib. If ax is specified,
+    add the plot to this ax instead of creating a new figure
 
     """
     import matplotlib.pyplot as plt
@@ -44,40 +65,91 @@ def Plot(F, unwrap = False, phaseblank =True, title='', circ_r=None, zoom=1):
     extent = [(xmin-0.5*dx)/units, (xmax+0.5*dx)/units,
               (xmax+0.5*dx)/units, (xmin-0.5*dx)/units]#lrbt
     
-    fig, axs = plt.subplots(1,2)
-    ax1, ax2 = axs
+    if ax is None:
+        fig, ax = plt.subplots(1, 1, **kwargs)
+        fig.suptitle(title)
+        ax.set_title('Intensity')
+    else:
+        if title:
+            ax.set_title(title)
+        else:
+            ax.set_title('Intensity')
+    
     I = Intensity(0,F)
-    Phi = Phase(F)
-    if unwrap:
-        Phi = PhaseUnwrap(Phi)
-    Imax = I.max()
-    Ithresh = Imax/256 #since screen cannot show difference then (in grayscale at least...)
-    if phaseblank:
-        Phi[I<Ithresh] = _np.nan
-    ax1.imshow(I, extent=extent)
-    ax1.set_xlabel(unit_txt)
-    ax1.set_xlim(xmin/zoom/units, xmax/zoom/units)
-    ax1.set_ylim(xmin/zoom/units, xmax/zoom/units)
+    
+    ax.imshow(I, extent=extent)
+    ax.set_xlabel(unit_txt)
+    ax.set_xlim(xmin/zoom/units, xmax/zoom/units)
+    ax.set_ylim(xmax/zoom/units, xmin/zoom/units) #y top is negative
     
     if circ_r is not None:
         circle = mpatches.Circle([0, 0], circ_r/units, fc='#ff000000',
                                  ec="grey", linestyle='--')
-        ax1.add_patch(circle)
-    im  = ax2.imshow(Phi, extent=extent)
-    ax2.set_xlim(xmin/zoom/units, xmax/zoom/units)
-    ax2.set_ylim(xmin/zoom/units, xmax/zoom/units)
+        ax.add_patch(circle)
+
+def PlotPhase(F, unwrap = False, phaseblank =True, circ_r=None,
+              zoom=1, title='', ax=None, ph_units='rad', **kwargs):
+    """
+    Plot the field intensity with matplotlib. If ax is specified,
+    add the plot to this ax instead of creating a new figure
+
+    """
+    import matplotlib.pyplot as plt
+    import matplotlib.patches as mpatches
+    
+    L = F.siz
+    xmin = -L/2
+    xmax = L/2
+    dx = F.dx
+    units = mm #[m] set if display should be in um/mm/...
+    unit_txt = 'mm'
+    if L < 3*mm:
+        units = um
+        unit_txt = 'um'
+    
+    extent = [(xmin-0.5*dx)/units, (xmax+0.5*dx)/units,
+              (xmax+0.5*dx)/units, (xmin-0.5*dx)/units]#lrbt
+    
+    if ax is None:
+        fig, ax = plt.subplots(1, 1, **kwargs)
+        fig.suptitle(title)
+    
+    blank_eps = 1/256 if phaseblank else 0
+    #since 8bit screen cannot show difference then (in grayscale at least...)
+    Phi = Phase(F, unwrap=unwrap, blank_eps=blank_eps, units=ph_units)
+    
+    if not unwrap:
+        im  = ax.imshow(Phi, extent=extent, vmin=-_np.pi, vmax=_np.pi)
+    else:
+        im  = ax.imshow(Phi, extent=extent)
+    ax.set_xlabel(unit_txt)
+    ax.set_xlim(xmin/zoom/units, xmax/zoom/units)
+    ax.set_ylim(xmax/zoom/units, xmin/zoom/units) #y min is top
+    ax.set_title('Phase map'+(' (wrapped)' if not unwrap else ' (unwrapped)'))
     
     if circ_r is not None:
         circle = mpatches.Circle([0, 0], circ_r/units, fc='#ff000000',
                                  ec="grey", linestyle='--')
-        ax2.add_patch(circle)
-    fig.colorbar(im)
-    if title:
-        ax1.set_title(title)
-    # return I, Phi #deprecated, now can easily be done with LPInt/LPPhase
+        ax.add_patch(circle)
+    cbar = plt.colorbar(im, ax=ax)
+    if ph_units=='rad':
+        cbar.ax.set_ylabel('phase angle [rad]')
+    elif ph_units=='lam':
+        cbar.ax.set_ylabel('phase delay [lambda]')
+    elif ph_units=='opd':
+        cbar.ax.set_ylabel('phase delay [opd]')
+    else:
+        cbar.ax.set_ylabel('phase delay')
+    if not unwrap:
+        cbar.set_ticks([-_np.pi, -_np.pi/2, 0, _np.pi/2, _np.pi])
+        cbar.ax.set_yticklabels(['-pi', '', '0', '', 'pi'])
+    else:
+        pass# would need more fancy way to find max etc to manually set
+        # correct ticks...
 
 
-def PlotCompareI(F1, F2, title='', circ_r=None, zoom=1):
+
+def PlotCompareI(F1, F2, title='', title1='', title2='', circ_r=None, zoom=1):
     """
     Plot the field intensity for Field 1 and 2 with coupled scales
 
@@ -143,7 +215,10 @@ def PlotCompareI(F1, F2, title='', circ_r=None, zoom=1):
         ax2.add_patch(circle)
     # fig.colorbar(im)
     if title:
-        ax1.set_title(title)
-    # return I, Phi #deprecated, now can easily be done with LPInt/LPPhase
-
+        fig.suptitle(title)
+    if title1:
+        ax1.set_title(title1)
+    if title2:
+        ax2.set_title(title2)
+    return fig
 
