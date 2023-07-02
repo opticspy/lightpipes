6f5bec42fc9a2edfd2528265d69dc687939c06cf
Leonard.Doyle <leonard.doyle@physik.uni-muenchen.de>
2023-07-02 17:55:23 +0200

    Added Wavefront_rms and Wavefront_ptv commands

--- LightPipes/misc.py
+++ LightPipes/misc.py
@@ -4,6 +4,7 @@ import numpy as _np
 import functools
 
 from .field import Field
+from .core import Phase
 
 PI=_np.pi
 def backward_compatible(fn):
@@ -253,3 +254,43 @@ def Tilt( Fin, tx, ty,):
     fi = -k*(tx*xx + ty*yy)
     Fout.field *= _np.exp(1j * fi)
     return Fout
+
+
+def Wavefront_ptv(F, units='opd'):
+    """
+    *Calculate the peak to valley (PtV) abberation of the wavefront of the
+    given field.*
+    
+    Only tested to work with a contiguous field in a centered
+    aperture with a phase that can be unwrapped without problems.
+    
+    :param units: as used in Phase(). Default is 'opd'
+    :return: The PtV abberation of the wavefront in specified units.
+    :rtype: float
+    """
+    pmap = Phase(F, unwrap=True, units=units, blank_eps=1e-3)
+    ptv = _np.nanmax(pmap) - _np.nanmin(pmap) #[rad]
+    return ptv
+
+
+def Wavefront_rms(F, units='opd'):
+    """
+    *Calculate the root-mean-square (rms) deviation of the wavefront of the
+    given field.*
+    
+    Only tested to work with a contiguous field in a centered
+    aperture with a phase that can be unwrapped without problems.
+    
+    :param units: as used in Phase(). Default is 'opd'
+    :return: The rms abberation of the wavefront in specified units.
+    :rtype: float
+    """
+    pmap = Phase(F, unwrap=True, units=units, blank_eps=1e-3)
+    mean = _np.nanmean(pmap)
+    pmap -= mean #set data to mean 0
+    target = _np.zeros_like(pmap) #compare-target is a flat phase front
+    targetflat = target[~_np.isnan(pmap)]
+    pmapflat = pmap[~_np.isnan(pmap)]
+    rms = _np.sqrt(((pmapflat - targetflat)** 2).mean())
+    return rms
+
